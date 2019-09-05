7f7b325003eef0b08eb88d93c3bd1bdddc3d928f
ramazan <ramazanb1995@mail.ru>
2019-09-06 01:00:35 +0300

    first classes

---
+++ src/main/java/com/kanasuki/nadbank/Main.java
@@ -0,0 +1,4 @@
+package com.kanasuki.nadbank;
+
+public class Main {
+}
---
+++ src/test/java/com/kanasuki/nadbank/MainTest.java
@@ -0,0 +1,13 @@
+package com.kanasuki.nadbank;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+
+public class MainTest {
+
+    @Test
+    public void shouldFailWhenCalled() {
+        assertTrue(false);
+    }
+}
